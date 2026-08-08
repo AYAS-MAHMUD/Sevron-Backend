8f686dad04af0740115e8529588ac7e2f4c6fe64
Mohammad Ayas <mohammadayas@MacBook-Air-M4.local>
2026-08-08 15:51:32 +0600

    Maked node mailer transporter and sendEmail function

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -15,6 +15,13 @@ const requiredEnv = [
   "JWT_REFRESH_EXPIRES_IN",
   "SUPER_ADMIN_EMAIL",
   "SUPER_ADMIN_PASSWORD",
+  "SMTP_HOST",
+  "SMTP_PORT",
+  "SMTP_USER",
+  "SMTP_PASS",
+  "SMTP_FROM"
+
+
 ] as const;
 
 requiredEnv.forEach((key) => {
@@ -36,4 +43,11 @@ export const config = Object.freeze({
   JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN,
   SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL,
   SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD,
+  smtp : {
+    host : process.env.SMTP_HOST,
+    port : process.env.SMTP_PORT,
+    smtp_user : process.env.SMTP_USER,
+    smtp_pass : process.env.SMTP_PASS,
+    smtp_from : process.env.SMTP_FROM
+  }
 });
---
+++ src/app/lib/email.ts
@@ -0,0 +1,57 @@
+
+import nodemailer from "nodemailer"
+import { config } from "../config/config"
+import path from "path"
+import ejs from "ejs"
+
+export const transporter = nodemailer.createTransport({
+  host: config.smtp.host,
+  port: Number(config.smtp.port),
+  secure: false, // true for 465, false for 587
+  auth: {
+    user: config.smtp.smtp_user,
+    pass: config.smtp.smtp_pass,
+  },
+});
+
+
+interface sendEmailOption {
+    to : string,
+    subject : string,
+    templateName : string,
+    templateData? : Record<string,any>
+    attachments? : {
+        filename : string,
+        content : Buffer | string,
+        contentType : string
+    }[]
+}
+
+export const sendEmail = async ({
+    to,
+    subject,
+    templateName,
+    templateData,
+    attachments
+}: sendEmailOption)=>{
+    try {
+    const templatePath = path.resolve(process.cwd() ,`src/app/templates/${templateName}.ejs`);
+    
+    const html = await ejs.renderFile(templatePath,templateData);
+
+    const info = await transporter.sendMail({
+        from : config.smtp.smtp_from,
+        to  : to,
+        subject : subject,
+        html : html,      // we use ejs for making template 
+        attachments : attachments?.map(attachment => ({
+            filename : attachment.filename,
+            content : attachment.content,
+            contentType : attachment.contentType
+        }))
+    })
+    console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
+    } catch (error) {
+        console.log("Email sending error", error)
+    }
+}
